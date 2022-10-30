43230addb4544393e7efd3edab82db930d4a8e1f
博麗靈夢 <1725338233@qq.com>
2022-10-30 21:27:43 +0800

    增加 CloudFlare IP 判断方法
    Signed-off-by: 博麗靈夢 <1725338233@qq.com>

--- src/main/java/com/mrx/dns/util/NetworkUtil.java
+++ src/main/java/com/mrx/dns/util/NetworkUtil.java
@@ -1,10 +1,30 @@
 package com.mrx.dns.util;
 
+import java.util.List;
+
 /**
  * @author Mr.X
  * @since 2022-10-30 21:22
  */
 public class NetworkUtil {
+
+    private static final List<String> CF_IPS = List.of(
+            "173.245.48.0/20",
+            "103.21.244.0/22",
+            "103.22.200.0/22",
+            "103.31.4.0/22",
+            "141.101.64.0/18",
+            "108.162.192.0/18",
+            "190.93.240.0/20",
+            "188.114.96.0/20",
+            "197.234.240.0/22",
+            "198.41.128.0/17",
+            "162.158.0.0/15",
+            "104.16.0.0/13",
+            "104.24.0.0/14",
+            "172.64.0.0/13",
+            "131.0.72.0/22"
+    );
 
     public static boolean isInRange(String ip, String cidr) {
         String[] ips = ip.split("\\.");
@@ -21,6 +41,13 @@ public class NetworkUtil {
                 | Integer.parseInt(cidrIps[3]);
 
         return (ipAddr & mask) == (cidrIpAddr & mask);
+    }
+
+    public static boolean isInCFips(String ip) {
+        for (String cidr : CF_IPS) {
+            if (isInRange(ip, cidr)) return true;
+        }
+        return false;
     }
 
 }
--- src/test/java/com/mrx/springdnsserver/TestWithOutSpring.java
+++ src/test/java/com/mrx/springdnsserver/TestWithOutSpring.java
@@ -11,8 +11,8 @@ public class TestWithOutSpring {
 
     @Test
     public void networkTest() {
-        System.out.println(NetworkUtil.isInRange("172.67.186.61", "173.245.48.0/20"));
-        System.out.println(NetworkUtil.isInRange("172.67.186.62", "172.64.0.0/13"));
+        System.out.println(NetworkUtil.isInCFips("172.67.186.61"));
+        System.out.println(NetworkUtil.isInCFips("172.67.186.62"));
     }
 
 }

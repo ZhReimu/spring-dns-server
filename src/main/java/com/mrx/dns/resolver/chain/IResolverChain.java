2ee119dd0635cbce4e8352ebc7c8a86325303621
博麗靈夢 <1725338233@qq.com>
2022-11-01 11:54:58 +0800

    去除重复代码
    Signed-off-by: 博麗靈夢 <1725338233@qq.com>

--- src/main/java/com/mrx/dns/resolver/chain/IResolverChain.java
+++ src/main/java/com/mrx/dns/resolver/chain/IResolverChain.java
@@ -1,27 +1,14 @@
 package com.mrx.dns.resolver.chain;
 
+import com.mrx.dns.repository.IHostRepository;
 import com.mrx.dns.resolver.IResolver;
-import lombok.SneakyThrows;
-
-import java.util.List;
-
-import static com.mrx.dns.util.PerformanceUtil.runMeasure;
 
 /**
  * @author Mr.X
  * @since 2022-11-01 11:40
  */
-public interface IResolverChain {
+public interface IResolverChain extends IHostRepository {
 
     void addResolver(IResolver... resolvers);
-
-    @SneakyThrows
-    default List<String> get(String host, String ip) {
-        // 去除 dns 查询的域名后缀 .
-        String nKey = host.endsWith(".") ? host.substring(0, host.length() - 1) : host;
-        return runMeasure(() -> getIpsByHost(nKey, ip));
-    }
-
-    List<String> getIpsByHost(String host, String ip);
 
 }

85d015546c24d32500e307192c94c17a7ab5c7af
博麗靈夢 <1725338233@qq.com>
2022-11-01 16:05:52 +0800

    修复 addDnsIps 导致触发 唯一索引异常 的问题
    Signed-off-by: 博麗靈夢 <1725338233@qq.com>

--- src/main/java/com/mrx/springdnsserver/model/dns/DnsIp.java
+++ src/main/java/com/mrx/springdnsserver/model/dns/DnsIp.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.Objects;
+
 /**
  * @author Mr.X
  * @since 2022-11-01 12:43
@@ -25,6 +27,19 @@ public class DnsIp {
 
     public static DnsIp of(String ip) {
         return new DnsIp(ip);
+    }
+
+    @Override
+    public int hashCode() {
+        return ip != null ? ip.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DnsIp dnsIp = (DnsIp) o;
+        return Objects.equals(ip, dnsIp.ip);
     }
 
 }
